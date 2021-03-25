809d70e232974f9bc6bdcb4a5a8368e8ce5f75f5
fanidc7 <fanidc7@gmail.com>
2021-03-25 14:11:29 +0700

    index product

---
+++ resources/views/pages/dashboard/product/index.blade.php
@@ -0,0 +1,34 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Product') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="mb-10">
+                <a href="{{ route('dashboard.product.create') }}" class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded shadow-lg">
+                   + Create Product
+                </a>
+            </div>
+            <div class="shadow overflow-hidden sm-rounded-md">
+                <div class="px-4 py-5 bg-white sm:p-6">
+                    <table id="crudtable">
+                        <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>Nama</th>
+                                <th>Harga</th>
+                                <th>Aksi</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
